c0c1ae01fb477ce0ac099527c5612a030d708476
mutska <mutska@ciencias.unam.mx>
2019-12-28 22:59:12 -0600

    1124 Celebrity Jeopardy. :)

---
+++ Competitive_Programming_3/1._Introduction/Getting_Started_The_Easy_Problems/Super_Easy/1124.cpp
@@ -0,0 +1,18 @@
+//2019-12-28 Celebrity Jeopardy by Mutska`
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	
+	string input_line;
+
+	while(getline(cin, input_line)){
+		
+		cout << input_line  << "\n";			
+			
+		}	
+
+	return 0;
+}
