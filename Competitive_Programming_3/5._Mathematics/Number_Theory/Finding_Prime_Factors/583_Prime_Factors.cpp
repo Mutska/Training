658fe6d993e1216c5b2a7be6158e1e16f5f95dbe
mutska <mutska@ciencias.unam.mx>
2020-04-16 13:40:03 -0500

    more news

--- Competitive_Programming_3/5._Mathematics/Number_Theory/Finding_Prime_Factors/583_Prime_Factors.cpp
+++ Competitive_Programming_3/5._Mathematics/Number_Theory/Finding_Prime_Factors/583_Prime_Factors.cpp
@@ -31,7 +31,7 @@ bool check_composite(u64 n, u64 a, u64 d, int s) {
     }
     return true;
 }
-bool MillerRabin(u64 n) { // returns true if n is prime, else returns false.
+bool MillerRabin(u64 n) { 
     if (n < 2)
         return false;
 
--- Competitive_Programming_3/7._Computational_Geometry/Basic_Geometry_Part_1/Points_and_Lines/378_Intersecting_Lines.cpp
+++ Competitive_Programming_3/7._Computational_Geometry/Basic_Geometry_Part_1/Points_and_Lines/378_Intersecting_Lines.cpp
@@ -7,42 +7,38 @@ using namespace std;
 #define INF 1e9
 #define EPS 1e-9
 
-struct point { double x, y;   // only used if more precision is needed
-  point() { x = y = 0.0; }                      // default constructor
-  point(double _x, double _y) : x(_x), y(_y) {}        // user-defined
-  bool operator < (point other) const { // override less than operator
-    if (fabs(x-other.x) > EPS)                   // useful for sorting
-      return x < other.x;          // first criteria , by x-coordinate
-    return y < other.y; }          // second criteria, by y-coordinate
-  // use EPS (1e-9) when testing equality of two floating points
+struct point { double x, y;   
+  point() { x = y = 0.0; }    
+  point(double _x, double _y) : x(_x), y(_y) {}       
+  bool operator < (point other) const { 
+    if (fabs(x-other.x) > EPS)          
+      return x < other.x;         
+    return y < other.y; }         
   bool operator == (point other) const {
    return (fabs(x-other.x) < EPS && (fabs(y-other.y) < EPS)); } };
 
- struct line { double a, b, c; };          // a way to represent a line
+ struct line { double a, b, c; };          
 
-// the answer is stored in the third parameter (pass by reference)
+
 void pointsToLine(point p1, point p2, line &l) {
-  if (fabs(p1.x-p2.x) < EPS)                  // vertical line is fine
-    l = {1.0, 0.0, -p1.x};                           // default values
+  if (fabs(p1.x-p2.x) < EPS)      
+    l = {1.0, 0.0, -p1.x};        
   else {
     double a = -(double)(p1.y-p2.y) / (p1.x-p2.x);
     l = {a,
-         1.0,              // IMPORTANT: we fix the value of b to 1.0
+         1.0,             
          -(double)(a*p1.x) - p1.y}; }
   }
 
-bool areParallel(line l1, line l2) {       // check coefficients a & b
+bool areParallel(line l1, line l2) {      
   return (fabs(l1.a-l2.a) < EPS) && (fabs(l1.b-l2.b) < EPS); }
 
-bool areSame(line l1, line l2) {           // also check coefficient c
+bool areSame(line l1, line l2) {          
   return areParallel(l1 ,l2) && (fabs(l1.c-l2.c) < EPS); }
 
-// returns true (+ intersection point) if two lines are intersect
 bool areIntersect(line l1, line l2, point &p) {
-  if (areParallel(l1, l2)) return false;            // no intersection
-  // solve system of 2 linear algebraic equations with 2 unknowns
+  if (areParallel(l1, l2)) return false;            
   p.x = (l2.b*l1.c - l1.b*l2.c) / (l2.a*l1.b - l1.a*l2.b);
-  // special case: test for vertical line to avoid division by zero
   if (fabs(l1.b) > EPS) p.y = -(l1.a*p.x + l1.c);
   else                  p.y = -(l2.a*p.x + l2.c);
   return true; }
