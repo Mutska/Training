ed8d9f2a270ba27755ad12c9b6f364e3a9b2fb2a
mutska <mutska@ciencias.unam.mx>
2020-05-06 16:12:25 -0500

    improved code

--- CP-Algorithms/Algebra/Prime_Numbers/Sieve_Of_Eratosthenes/Printing_Some_Primes.cpp
+++ CP-Algorithms/Algebra/Prime_Numbers/Sieve_Of_Eratosthenes/Printing_Some_Primes.cpp
@@ -13,10 +13,11 @@ bitset<100000010> bs;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int j = 0;                              
+    cout << "2\n";
+    int j = 1;                              
     _sieve_size = 100000001;                     
      bs.set();                                       
-    for (ll i = 2; i < _sieve_size; i++) if (bs[i]) {
+    for (ll i = 3; i < _sieve_size; i+=2) if (bs[i]) {
     for (ll j = i*i; j < _sieve_size; j += i) bs[j] = 0;
     if(j % 100 == 0)
       cout << i << "\n";
