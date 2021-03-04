8e6791804bc31168e57708f52f0cf1687aacc1b3
mutska <mutska@ciencias.unam.mx>
2021-03-03 19:05:06 -0600

    Fixing

--- Competitive_Programming_4/3._Problem_Solving_Paradigms/Complete_Search/Recursive_Backtracking_Easy/524_Prime_Ring_Problem.cpp
+++ Competitive_Programming_4/3._Problem_Solving_Paradigms/Complete_Search/Recursive_Backtracking_Easy/524_Prime_Ring_Problem.cpp
@@ -2,93 +2,51 @@
 #include <vector>
 using namespace std;
 int N, Case = 0;
-vector<int> solutions;
+/* vector que guarda la solucion parcial encontrada en un momento dado
+   y extendiendo la solucion parcial hasta encontrar  una solucion completa*/
+vector<int> solutions; 
 
-using u64 = unsigned long long int;
-u64 binpower(u64 base, u64 e, u64 mod) {
-    u64 result = 1;
-    base %= mod;
-    while (e) {
-        if (e & 1)
-            result = (u64)result * base % mod;
-        base = (u64)base * base % mod;
-        e >>= 1;
-    }
-    return result;
+int primes[11] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+
+bool is_prime(int posibble_prime){
+    for(int &p: primes)
+        if(posibble_prime == p) return true;
+    return false;
 }
-
-bool check_composite(u64 n, u64 a, u64 d, int s) {
-    u64 x = binpower(a, d, n);
-    if (x == 1 || x == n - 1)
-        return false;
-    for (int r = 1; r < s; r++) {
-        x = (u64)x * x % n;
-        if (x == n - 1)
-            return false;
-    }
-    return true;
-};
-
-
-bool  MillerRabinDeterministic(u64 n) { 
-    if (n < 2)
-        return false;
-    int r = 0;
-    u64 d = n - 1;
-    while ((d & 1) == 0) {
-        d >>= 1;
-        r++;
-    }
-
-    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
-        if (n == a)
-            return true;
-        if (check_composite(n, a, d, r))
-            return false;
-    }
-    return true;
-}
-
-bool canAdd(int n) {
+/*Funcion auxiliar que nos indica si el elemento actual
+  puede ser considerado como parte de la solucion */
+bool canAdd(int n) { 
     for(auto &s: solutions)
         if(s == n) return false;
     int possible_prime = n + solutions.back();
-    cout << "possible prime is: " << possible_prime << "\n";
-    if(MillerRabinDeterministic(possible_prime)) return true;
+    if(is_prime(possible_prime)) return true;
     return false;
 }
 
 void backtrack() {
-    cout << "N is: " << N << "\n";
-    cout << "size is: " << solutions.size() << "\n";
-    if(solutions.size() == N) {
+    if(solutions.size() == N) { //Verificamos si ya encontramos una solucion
         int check = solutions.back() + 1;
-        if(!(MillerRabinDeterministic(check))) return;
+        if(!(is_prime(check))) return;
         cout << solutions[0];
         for(int i = 1; i < N; ++i) cout << " " << solutions[i];
         cout << "\n";
     }
-    for(int i = 2; i <= N; ++i) {
-        cout << "i is: " << i << "\n";
-        if(canAdd(i)){
-            cout << "in fact " << i  << " with " << solutions.back() << " is prime\n";
-            solutions.push_back(i);
-            backtrack();
-            solutions.pop_back();
+    for(int i = 2; i <= N; ++i) { // Recorremos el espacio de desiciones
+        if(canAdd(i)){ 
+            solutions.push_back(i); // Metemos el elemento actual
+            backtrack();           // Hacemos recursion
+            solutions.pop_back(); // Sacamos el elemento 
         }
     }
 }
 
 int main() {
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);
-    cin >> N;
-    solutions.push_back(1);
-    cout << "Case " << ++Case << ":\n";
-    backtrack();
-    solutions.clear();
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n = 0;
     while(cin >> N) {
-        if(!cin.eof()) cout << "\n";
+        if (n > 0) cout << "\n";
+        ++n;
         solutions.push_back(1);
         cout << "Case " << ++Case << ":\n";
         backtrack();
